639429586c306393d4b7dfc0a696ca5f0d5f0d33
Himanshu <himanshumude01@gmail.com>
2022-10-26 03:31:21 +0530

    Update

--- bot.js
+++ bot.js
@@ -1,7 +1,51 @@
+const fs = require('node:fs');
+const path = require('node:path');
 require('dotenv').config();
-const { Client, Events, GatewayIntentBits } = require('discord.js');
+const { Client, Collection, Events, GatewayIntentBits } = require('discord.js');
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+client.commands = new Collection();
 client.once(Events.ClientReady, () => {
     console.log("Logged In!!!");
+});
+client.on(Events.InteractionCreate, async interaction => {
+    if (!interaction.isChatInputCommand()) return;
+
+    const { commandName } = interaction;
+
+    if (commandName === 'ping') {
+        await interaction.reply('Pong!');
+    } else if (commandName === 'beep') {
+        await interaction.reply('Boop!');
+    }
+});
+const commandsPath = path.join(__dirname, 'commands');
+const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+    const filePath = path.join(commandsPath, file);
+    const command = require(filePath);
+    // Set a new item in the Collection with the key as the command name and the value as the exported module
+    if ('data' in command && 'execute' in command) {
+        client.commands.set(command.data.name, command);
+    } else {
+        console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+    }
+}
+client.on(Events.InteractionCreate, interaction => {
+    if (!interaction.isChatInputCommand()) return;
+
+    const command = interaction.client.commands.get(interaction.commandName);
+
+    if (!command) {
+        console.error(`No command matching ${interaction.commandName} was found.`);
+        return;
+    }
+
+    try {
+        command.execute(interaction);
+    } catch (error) {
+        console.error(error);
+        interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+    }
 });
 client.login(process.env.TOKEN);
