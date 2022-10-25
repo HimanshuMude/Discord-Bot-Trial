2dc93b404772cc398a5be3f744a904afda3deae3
Himanshu <himanshumude01@gmail.com>
2022-10-26 00:04:38 +0530

    temp

---
+++ bot.js
@@ -0,0 +1,7 @@
+require('dotenv').config();
+const Discord = require('discord.js');
+const client = new Discord.Client();
+client.on('ready', () => {
+    console.log("Logged In!!!");
+});
+client.login(process.env.TOKEN);
